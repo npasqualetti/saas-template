ac1e1c2511679a09bd28766edf81c61d97b593ae
nick p <nicholas.pasqualetti@gmail.com>
2023-05-13 19:39:04 -0400

    watchlist update

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -47,11 +47,12 @@ export default async function IndexPage() {
             Follow along on Twitter
           </Link>
           <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
-            An example app built using Next.js 13 server components.
+            Unleash the power of sentiment AI with CounteractAI!
           </h1>
           <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
-            I&apos;m building a web app with Next.js 13 and open sourcing
-            everything. Follow along as we figure this out together.
+            Maximize your investment potential, make informed decisions, and
+            seize opportunities like never before. Join the Sentisense
+            revolution today.
           </p>
           <div className="space-x-4">
             <Link href="/login" className={cn(buttonVariants({ size: "lg" }))}>
