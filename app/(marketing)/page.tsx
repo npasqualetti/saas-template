621c8a74533483c8bb6b052597db7694c252bea8
nick p <nicholas.pasqualetti@gmail.com>
2023-05-15 06:27:03 -0400

    layout update

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -6,7 +6,6 @@ import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 
 export default async function IndexPage() {
-
   return (
     <>
       <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
@@ -28,7 +27,7 @@ export default async function IndexPage() {
           </p>
           <div className="space-x-4">
             <Link href="/login" className={cn(buttonVariants({ size: "lg" }))}>
-              Get Started
+              Get Started!
             </Link>
           </div>
         </div>
--- config/dashboard.ts
+++ config/dashboard.ts
@@ -14,11 +14,6 @@ export const dashboardConfig: DashboardConfig = {
   ],
   sidebarNav: [
     {
-      title: "Dashboard",
-      href: "/dashboard",
-      icon: "post",
-    },
-    {
       title: "Billing",
       href: "/dashboard/billing",
       icon: "billing",
